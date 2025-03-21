47f6de46226895a8aeb98779e9baabdc13b9824d
Blair Crumbly <blaircrumbly@gmail.com>
2025-03-21 17:51:42 -0400

    one class has many tasks relationship implemented

--- server/models/klass.py
+++ server/models/klass.py
@@ -5,6 +5,7 @@ class Klass(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
     description = db.Column(db.String(30))
+    
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     tasks = db.relationship("Task", back_populates='classes')
     def __repr__(self):
--- server/models/task.py
+++ server/models/task.py
@@ -8,9 +8,11 @@ class Task(db.Model):
     description = db.Column(db.String)
     due_date = db.Column(db.DateTime, nullable=False)
     completed = db.Column(db.DateTime, nullable=True)
-    class_id = db.Column(db.Integer, db.ForeignKey('classes.id'), nullable=False)
+
     # tasks relationship to categories through category_tasks
     categories = db.relationship('Category', secondary='category_tasks', back_populates='tasks')
+    # many part of one class having many tasks
+    class_id = db.Column(db.Integer, ForeignKey='classes.id', back_populates='tasks', nullable=False)
 
 
     def __repr__(self):
